98b51e44835747ba0e9637970eca6f828f47dc63
Edouard Meurant <edouard@MacBookPro.fritz.box>
2024-12-02 17:27:58 +0100

    modf lock2

--- Sourcefile/testANDset.c
+++ Sourcefile/testANDset.c
@@ -18,34 +18,21 @@ void verrou_lock(int *verrou){
 }
 
 
-void verrou_lock2(int *verrou){
-
+void verrou_lock2(int *verrou) {
     __asm__ __volatile__(
         "start:                     \n"
-            "movl $1, %%eax;     \n"
-            "xchgl %%eax, %[verrou];     \n"
-            "cmpl %%eax, %%eax;          \n"
-            "jne cache;                      \n"
-            "je fin;                          \n"
-
-
-        "cache:                        \n"
-            "cmpl $0, %[verrou];     \n"      
-            "je start;                      \n"
-             "jne cache;                      \n"
-        "fin:                                 \n"     
-
-
-
-
-
-            
+            "movl $1, %%eax;         \n"  // Charge 1 dans %eax
+            "xchgl %%eax, %[verrou]; \n"  // Échange atomique entre %eax et verrou
+            "testl %%eax, %%eax;     \n"  // Test si verrou == 0
+            "jne cache;              \n"  // Si verrou pris, passe en lecture passive
+        "cache:                     \n"
+            "cmpl $0, %[verrou];     \n"  // Vérifie si verrou == 0
+            "jne cache;              \n"  // Si verrou toujours pris, boucle
+            "jmp start;              \n"  // Sinon, retente de l'acquérir
             :
-            : [verrou] "m" (*verrou)
-            : "%eax", "memory"
-
+            : [verrou] "m" (*verrou)       // Entrée : verrou (en mémoire)
+            : "%eax", "memory"             // Clobbers : %eax et mémoire
     );
-
 }
 
 void verrou_unlock(int *verrou) {
